048b5fa293997ecd6881cc37b467d411d298f630
UGKONG <jsw9330@icloud.com>
2022-03-16 21:46:09 +0900

    3.0.15

--- modules/hooks/useStore.js
+++ modules/hooks/useStore.js
@@ -2,7 +2,6 @@ import create from "zustand";
 
 const store = create(set => ({
   number: 0,
-  string: 'JSW',
   setState: (type, payload) => set(state => {
     if (!type) return console.warn('type is not found!!');
     if (typeof(type) !== 'string') return console.warn('type is not string!!');
